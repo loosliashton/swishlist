a939ff0993d5a6d42a677e6a0032171f323b95d2
Ashton Loosli <57840670+loosliashton@users.noreply.github.com>
2025-11-22 15:26:57 -0700

    Rebranded to Swishlist and set up Firebase (#1)
    * Rebranding
    
    * Rename dev web app

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      
+
     ],
     client: {
       jasmine: {
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/christmas-list'),
+      dir: require('path').join(__dirname, './coverage/swishlist'),
       subdir: '.',
       reporters: [
         { type: 'html' },
