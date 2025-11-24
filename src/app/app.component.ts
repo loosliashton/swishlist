5bbe7df5ab2545e8e6c6607d69c89cfbf8fbd24c
loosliashton <loosliashton@gmail.com>
2025-11-24 13:16:02 -0700

    Check for new versions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { filter } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: false,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'swishlist';
+
+  constructor(
+    private swUpdate: SwUpdate,
+    private snackBar: MatSnackBar,
+  ) {}
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates
+        .pipe(
+          filter(
+            (evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY',
+          ),
+        )
+        .subscribe(() => {
+          const snack = this.snackBar.open('New version available', 'Reload');
+          snack.onAction().subscribe(() => {
+            this.swUpdate
+              .activateUpdate()
+              .then(() => window.location.reload())
+              .catch((err) => console.error('Failed to activate update', err));
+          });
+        });
+    }
+  }
 }
